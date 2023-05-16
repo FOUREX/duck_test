750d6d54f889f84cbd92898d2e6dd53eb8a67688
FOUREX <Foxtrotserega@gmail.com>
2023-05-16 09:52:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+    int iResult;
+
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        std::cout << "Error, lol" << std::endl;
+        return 1;
+    }
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+}
